e39fe0e5cd5b4ddcbee0583eef3c9d25e2be4955
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-25 03:06:34 +0530

    new

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -3,21 +3,22 @@ import Navbar from "../components/Navbar";
 import Hero from "../sections/AboutSections/Hero";
 import About from "../sections/AboutSections/About";
 import RoadmapCards from "../components/RoadmapCards";
-import Tokenomics from "../sections/Tokenomics";
+
 import Footer from "../sections/Footer";
 import Team from "../sections/AboutSections/Team";
 import Roadmap from "../sections/Roadmap";
-import Navbar2 from "../components/Navbar2";
+
+import Tokenomicss from "../sections/Tokenomicss";
 
 function AboutUs() {
   return <>
   <div>
-   <Navbar2/>
+   <Navbar/>
     <Hero/>
     <About/>
     <Roadmap/>
     <RoadmapCards/>
-    <Tokenomics/>
+    <Tokenomicss/>
     <Team/>
     <Footer/>
   </div>
