b76d21e5f40e81c2a2d398c874ad521b8a3ed992
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-24 17:03:32 +0530

    new

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -6,11 +6,11 @@ function Features() {
     <section className=" pb-[4rem] pt-[2rem] bg-white">
       <div>
         <Marquee pauseOnHover={true} loop={0}>
-          <img src="images/fetch/f1.png" alt="" className=" m-4 h-[320px]" />
-          <img src="images/fetch/f2.png" alt="" className=" m-4 h-[320px]" />
-          <img src="images/fetch/f3.png" alt=""  className=" m-4 h-[320px]"/>
-          <img src="images/fetch/f4.png" alt="" className=" m-4 h-[320px]" />
-          <img src="images/fetch/f5.png" alt="" className=" m-4 h-[320px]" />
+          <img src="images/fetch/f1.png" alt="" className=" m-4 h-[320px] bg-white p-6  shadow-md rounded-lg border border-[#67f062]" />
+          <img src="images/fetch/f2.png" alt="" className=" m-4 h-[320px] bg-white p-6  shadow-md rounded-lg border border-[#67f062]" />
+          <img src="images/fetch/f3.png" alt=""  className=" m-4 h-[320px] bg-white p-6  shadow-md rounded-lg border border-[#67f062]"/>
+          <img src="images/fetch/f4.png" alt="" className=" m-4 h-[320px] bg-white p-6  shadow-md rounded-lg border border-[#67f062]" />
+          <img src="images/fetch/f5.png" alt="" className=" m-4 h-[320px] bg-white p-6  shadow-md rounded-lg border border-[#67f062]" />
         </Marquee>
       </div>
     </section>
